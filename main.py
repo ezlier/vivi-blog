790db13237daea48885c71dde789c4351cf7947c
ezlier <86654833@qq.com>
2026-09-13 15:31:46 +0800

    神了

--- main.py
+++ main.py
@@ -45,5 +45,5 @@ setup_openapi(app)
 
 app.include_router(router)
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+# if __name__ == '__main__':
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000)
